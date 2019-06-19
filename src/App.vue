84350331c83def644429426a60d28bf049031728
sarab78 <sarbat_kaur@hotmail.co.uk>
2019-06-19 16:21:32 +0100

    shopping_list_classLab

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+  <h1>My Shopping List</h1>
+  <ul>
+<shopping-item v-for="(item, index) in items"  :key="index" :item="item"></shopping-item>
+</ul>
+</div>
+</template>
+
+<script>
+
+import ShoppingItem from './components/ShoppingItem'
+
+export default {
+  name: 'app',
+  components: {
+    'shopping-item': ShoppingItem
+  },
+data(){
+  return {
+    items: [
+      {name: 'Milk', purchased: false},
+      {name: 'Cheese', purchased: true},
+      {name: 'Beans', purchased: false}
+    ]
+  }
+}
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ShoppingItem.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div>
+  <p :class="{'purchased': item.purchased}">{{item.name}}</p>
+  <button v-if="!item.purchased" v-on:click="purchase">Purchase</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'shopping-item',
+  props: ['item'],
+  methods: {
+    purchase: function(){
+      this.item.purchased = true
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+p {
+  background-color: lightgoldenrodyellow;
+
+}
+.purchased {
+  text-decoration: line-through;
+  background-color: red;
+}
+</style>
